0885c68fd19231bd5af6eae6f046c2c2f4541b9c
unknown <socniklps@gmail.com>
2023-12-27 16:56:51 +0200

    Fix jwt-decode import issue

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -11,7 +11,7 @@
 
 import { useSelector } from "react-redux";
 import { selectCurrentToken } from "../features/auth/authSlice";
-import jwt_decode from "jwt-decode";
+import jwtDecode from "jwt-decode";
 
 const useAuth = () => {
   const token = useSelector(selectCurrentToken);
@@ -20,17 +20,18 @@ const useAuth = () => {
   let status = "Employee";
 
   if (token) {
-    const decoded = jwt_decode(token);
+    const decoded = jwtDecode(token);
     const { username, roles } = decoded.UserInfo;
+
     isManager = roles.includes("Manager");
     isAdmin = roles.includes("Admin");
+
     if (isManager) status = "Manager";
     if (isAdmin) status = "Admin";
 
     return { username, roles, status, isManager, isAdmin };
   }
 
-  return { usename: "", roles: [], isManager, isAdmin, status };
+  return { username: "", roles: [], isManager, isAdmin, status };
 };
-
 export default useAuth;
